1eeefd1ab76394d9586b08f9c9a54f8967e7e71e
pedrocatelli <pepecatelli@gmail.com>
2019-09-05 12:31:47 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
 	}
 
 }
