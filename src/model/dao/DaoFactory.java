9affb2959ba313cc332d07d79858c1ede5012525
pedrocatelli <pepecatelli@gmail.com>
2019-09-05 11:14:44 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
 import model.dao.impl.SellerDaoJDBC;
+import db.DB;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao(){
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
